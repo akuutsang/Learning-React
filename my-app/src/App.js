5eb0a551d3b355f63563413f7acbf66e31771f2f
akutsang <akuutsang@gmail.com>
2023-09-27 13:13:24 +0100

    react hooks, using useState to change values

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,25 @@
 import Employee from "./component/employee/Employee"
+import { useState } from "react" 
+
 function App() {
   const showEmployee =true;
+  const [role, setRole] = useState("dev");
+
   return (
     <div className="App ">
       {showEmployee ? (
       <>
+        <input type='text' onChange={(e)=>{
+          console.log(e.target.value)
+          setRole(e.target.value)
+        }}/>
+
         <Employee name="Abi" role="intern" />
         <Employee name="Abok" role="intern"/>
-        <Employee name="Akutsang" role=""/>
+        <Employee name="Akutsang" role={role}/>
         <Employee name="Akare" role="intern"/>
       </>
       ): ("access denied!")}
-      
-
     </div>
   );
 }
