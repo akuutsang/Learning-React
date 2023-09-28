ace5d2d4e612cdae6ee3439ae8310d2618ae942b
akutsang <akuutsang@gmail.com>
2023-09-28 13:36:38 +0100

    use props to style cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,31 @@
+import "./App.css"
 import Employee from "./component/employee/Employee"
 import { useState } from "react" 
+import Abi from "./Assets/Images/Audrey.jpeg"
+import Abok from "./Assets/Images/saboy.jpeg"
+import Akutsang from "./Assets/Images/akutsang.png"
+import Akare from "./Assets/Images/Tori.jpeg"
 
 function App() {
   const showEmployee =true;
   const [role, setRole] = useState("dev");
 
   return (
-    <div className="App ">
-      {showEmployee ? (
+    <div className="App">
+    {showEmployee ? (
       <>
         <input type='text' onChange={(e)=>{
           setRole(e.target.value)
         }}/>
-
-        <Employee name="Abi" role="intern" />
-        <Employee name="Abok" role="intern"/>
-        <Employee name="Akutsang" role={role}/>
-        <Employee name="Akare" role="intern"/>
+        <div className="flex flex-wrap">
+          <Employee name="Abi" role="Project Manager" img={Abi}/>
+          <Employee name="Abok" role="FrontEnd"  img={Abok}/>
+          <Employee name="Akutsang" role={role} img={Akutsang}/>
+          <Employee name="Akare" role="BackEnd" img={Akare}/>
+        </div>
       </>
-      ): ("access denied!")}
+      ): ("access denied!")} 
+      
     </div>
   );
 }
