d5c22b9fd98e14d7dced1de5b09dfb53d7fc4980
akutsang <akuutsang@gmail.com>
2023-09-29 14:00:36 +0100

    using objects to render props and using uuid key value

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,58 @@
-import "./App.css"
-import Employee from "./component/employee/Employee"
+import "./index.css"
 import { useState } from "react" 
-import Abi from "./Assets/Images/Audrey.jpeg"
-import Abok from "./Assets/Images/saboy.jpeg"
-import Akutsang from "./Assets/Images/akutsang.png"
-import Akare from "./Assets/Images/Tori.jpeg"
+import Employee from "./component/employee/Employee"
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
   const showEmployee =true;
   const [role, setRole] = useState("dev");
+  const  [employees, setEmployees] = useState([
+    {
+    name:"Abi",
+    role:"Project Manager", 
+    img:"/Assets/Images/Audrey.jpeg",
+    },
+    {
+    name:"Abok",
+    role:"FrontEnd", 
+    img:"/Assets/Images/saboy.jpeg",
+    },
+    {
+     name:"Akutsang",
+     role:"", 
+     img:"/Assets/Images/akutsang.png",
+    },
+    {
+     name:"Abi",
+     role:"BackEnd", 
+     img:"/Assets/Images/Tori.jpeg",
+    },
+  ]);
+
 
   return (
     <div className="App">
     {showEmployee ? (
       <>
-        <input type='text' onChange={(e)=>{
-          setRole(e.target.value)
-        }}/>
-        <div className="flex flex-wrap">
-          <Employee name="Abi" role="Project Manager" img={Abi}/>
-          <Employee name="Abok" role="FrontEnd"  img={Abok}/>
-          <Employee name="Akutsang" role={role} img={Akutsang}/>
-          <Employee name="Akare" role="BackEnd" img={Akare}/>
+        <input 
+          type='text' 
+          onChange={(e)=>{
+             setRole(e.target.value);
+          }}
+        />
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {
+            console.log(uuidv4())
+            return(
+              <Employee 
+                key={uuidv4()}
+                name={employee.name}
+                role={employee.role} 
+                img={employee.img}              
+                />
+              );
+          })}
         </div>
       </>
       ): ("access denied!")} 
