08c7e76bf351ddd5d2422c6d15f0e4326baf0d3b
akutsang <akuutsang@gmail.com>
2023-09-29 14:47:15 +0100

    add bootstrap and bootstrap modal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [role, setRole] = useState("dev");
   const  [employees, setEmployees] = useState([
     {
+      name:"Julius",
+      role:"", 
+      img:"/Assets/Images/julz.jpeg",
+      },
+    {
     name:"Abi",
     role:"Project Manager", 
     img:"/Assets/Images/Audrey.jpeg",
@@ -41,12 +46,17 @@ function App() {
              setRole(e.target.value);
           }}
         />
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center ">
           {employees.map((employee) => {
-            console.log(uuidv4())
+            // console.log(uuidv4())
             return(
               <Employee 
                 key={uuidv4()}
+                // to set a unique key without having to manually type it
+                // simply install uuid on the terminal i.e npm install uuid
+                // import it on the js file i.e import { v4 as uuidv4 } from 'uuid';
+                // then pass it as the key value i.e key{uuidv4}
+                // you can console.log it to view the unique key value on your console.
                 name={employee.name}
                 role={employee.role} 
                 img={employee.img}              
