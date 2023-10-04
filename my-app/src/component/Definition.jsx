a0d2850dc77e4d5793204ff4e70f3e4594c91366
akutsang <akuutsang@gmail.com>
2023-10-05 00:58:15 +0100

    remember the word here is hard coded and has terrible ux

--- my-app/src/component/Definition.jsx
+++ my-app/src/component/Definition.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Definition() {
-  const [word, setWord] = useState([]);
+  const [word, setWord] = useState();
 
   useEffect(() => {
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/helicopter")
@@ -30,30 +30,3 @@ export default function Definition() {
     </>
   );
 }
-
-// import { useState, useEffect } from "react";
-
-// export default function Definition() {
-//   const [word, setWord] = useState([]); // Initialize as an empty array
-//   useEffect(() => {
-//     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/helicopter")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setWord(data[0]?.meanings || []); // Use optional chaining and provide a default empty array
-//         console.log(data[0]?.meanings);
-//       });
-//   }, []);
-//   return (
-//     <>
-//       <h1>Here is a definition</h1>
-//       {word.map((meaning) => {
-//         return (
-//           <p>
-//             {meaning.partOfSpeech + ":"}
-//             {meaning.definitions[0].definition}
-//           </p>
-//         );
-//       })}
-//     </>
-//   );
-// }
