646d4d3ab043c2cf3603a997eda6b66031713d07
akutsang <akuutsang@gmail.com>
2023-10-04 23:22:04 +0100

    no dependency array

--- my-app/src/component/Dictionary.jsx
+++ my-app/src/component/Dictionary.jsx
@@ -3,18 +3,29 @@ import { useState, useEffect } from "react";
 
 export default function Dictionary() {
   const [word, setWord] = useState(" ");
+  const [word2, setWord2] = useState(" ");
 
-  useEffect(() => {});
+  useEffect(() => {
+    console.log("state updated", word + " " + word2);
+  });
+
+  // no dependency array --> update for anystate change asin line 7-9
   return (
     <>
       <input
         type="text"
         onChange={(e) => {
           setWord(e.target.value);
-          console.log("state updated", word);
         }}
       />
       <h1>Lets get the defination for {word}</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord2(e.target.value);
+        }}
+      />
+      <h2>Lets get the defination for {word2}</h2>
     </>
   );
 }
