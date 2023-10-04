55d600cdc571907e19af144f5423708d6d8fa85e
akutsang <akuutsang@gmail.com>
2023-10-04 23:39:18 +0100

    passing in data

--- my-app/src/component/Dictionary.jsx
+++ my-app/src/component/Dictionary.jsx
@@ -6,12 +6,18 @@ export default function Dictionary() {
   const [word2, setWord2] = useState(" ");
 
   useEffect(() => {
-    console.log("state updated", word + " " + word2);
+    console.log("state updated " + word);
   }, [word]);
+  // when word is passed in as the array, use effect will only take word as a dependency, ignoring word2
 
   // no dependency array --> update for anystate change asin line 7-9
 
   // empty dependency array --> execute once
+  useEffect(() => {
+    console.log("state updated " + word2);
+  }, [word2]);
+
+  // passing in data --> only execute when those state variables are changed
   return (
     <>
       <input
