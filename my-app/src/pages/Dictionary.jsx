7576eb2b5cfa2fe3170e58e2c13e6a6048394d93
akutsang <akuutsang@gmail.com>
2023-10-05 01:40:25 +0100

    use of useNavigate()

--- my-app/src/pages/Dictionary.jsx
+++ my-app/src/pages/Dictionary.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Dictionary() {
   const [word, setWord] = useState(" ");
-  const [word2, setWord2] = useState(" ");
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log("state updated " + word);
-  }, [word]);
-  // when word is passed in as the array, use effect will only take word as a dependency, ignoring word2
-
-  // no dependency array --> update for anystate change asin line 7-9
-
-  // empty dependency array --> execute once
-  useEffect(() => {
-    console.log("state updated " + word2);
-  }, [word2]);
-
-  // passing in data --> only execute when those state variables are changed
   return (
     <>
       <input
@@ -26,14 +14,14 @@ export default function Dictionary() {
           setWord(e.target.value);
         }}
       />
-      <h1>Lets get the defination for {word}</h1>
-      <input
-        type="text"
-        onChange={(e) => {
-          setWord2(e.target.value);
+
+      <button
+        onClick={() => {
+          navigate("/definition/" + word);
         }}
-      />
-      <h2>Lets get the defination for {word2}</h2>
+      >
+        Search
+      </button>
     </>
   );
 }
