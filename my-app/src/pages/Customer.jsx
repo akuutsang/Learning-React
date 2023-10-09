873afc59c7a96af97d83b9ec1eb046aa2d6eb7b7
akutsang <akuutsang@gmail.com>
2023-10-09 01:43:42 +0100

    useNavigate() to redirect

--- my-app/src/pages/Customer.jsx
+++ my-app/src/pages/Customer.jsx
@@ -1,13 +1,20 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function Customer() {
+  const navigate = useNavigate();
   const [customer, setCustomer] = useState();
+
   const { id } = useParams();
   useEffect(() => {
     const url = "http://localhost:8000/api/customers/" + id;
     fetch(url)
       .then((response) => {
+        if (response.status === 404) {
+          // redirect to a 404 page (new  URL required)
+          navigate("/404");
+          // render a 404 component in this page
+        }
         return response.json();
       })
       .then((data) => {
