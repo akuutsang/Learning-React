089e612dd4864acb0e13f41ff7a95feaa41ad99d
akutsang <akuutsang@gmail.com>
2023-10-19 02:24:24 +0100

    set login time

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,45 @@ import Employees from "./pages/Employees";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Customers from "./pages/Customers";
 import Customer from "./pages/Customer";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Dictionary from "./pages/Dictionary";
 import Definition from "./component/Definition";
 import NotFound from "./component/NotFound";
 import Count from "./component/Count";
 import Login from "./pages/Login";
+import { baseUrl } from "./Shared";
 
 export const LoginContext = createContext();
 
 function App() {
   // long term goal --> use refresh token, if it works, stay logged in, otherwise send to login page
+  useEffect(() => {
+    function refreshTokens() {
+      if (localStorage.refresh) {
+        const url = baseUrl + "api/token/refresh/";
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          }),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            localStorage.access = data.access;
+            localStorage.refresh = data.refresh;
+            setLoggedIn(true);
+          });
+      }
+    }
+    const minutes = 60000 * 15;
+    refreshTokens();
+    setInterval(refreshTokens, minutes * 3);
+  }, []);
   const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
   function changeLoggedIn(value) {
     setLoggedIn(value);
