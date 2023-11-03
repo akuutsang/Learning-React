29239c779c4612d92fe9502d457276c23d3eefd2
akutsang <akuutsang@gmail.com>
2023-11-03 01:11:56 +0100

    introducing componentDidMount()

--- my-app/src/component/LifeCycle.jsx
+++ my-app/src/component/LifeCycle.jsx
@@ -1,26 +1,72 @@
 import React, { Component } from "react";
 
 export default class LifeCycle extends Component {
-  //   const [item, setItem] = useState();
-
   state = {
-    character: {},
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isFriendly: "true",
+    employment: "",
+    favColor: "",
+  };
+
+  handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({
+          [name]: checked,
+        })
+      : this.setState({
+          [name]: value,
+        });
   };
 
   componentDidMount() {
     console.log("componentDidMount");
-    fetch("https://swapi.dev/api/people/5")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ character: data });
-      });
+    // fetch("https://swapi.dev/api/people/1")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     this.setState({ character: data });
+    //   });
   }
+
+  componentDidUpdate() {
+    console.log("update");
+  }
+
   render() {
     return (
-      <div>
-        <h1>Star wars</h1>
-        <h1>{this.state.character.name}</h1>
-      </div>
+      <form>
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={this.handleChange}
+          name="firstName"
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+          name="lastName"
+          value={this.state.lastName}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={this.handleChange}
+          name="email"
+          value={this.state.email}
+        />
+        <input
+          type="text"
+          placeholder="comments"
+          onChange={this.handleChange}
+          name="comments"
+          value={this.state.comments}
+        />
+      </form>
     );
   }
 }
